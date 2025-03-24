bc24b8861f855c80927dd30602944e18ccd1f0f5
Batchpingu <lamle96@gmail.com>
2025-03-24 05:25:42 -0400

    adjusted file path and fix lang to be list instead of strs

--- backend/app.py
+++ backend/app.py
@@ -2,22 +2,29 @@ import json
 import os
 from flask import Flask, render_template, request
 from flask_cors import CORS
-from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
+# from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
-
+from helpers import ranker
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
 os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
+
+
 
 # Get the directory of the current script
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, "db/sample_data.json")
+# json_file_path = os.path.join(current_directory, "db/sample_data.json")
+json_file_path = os.path.join(current_directory, "..", "db", "sample_data.json")
+
+
+
 
 top_10_langs = {'javascript', 'python', 'java', 'typescript', 'csharp', 'cpp', 'php', 'shell', 'c', 'ruby'}
 with open(json_file_path, 'r') as file:
     data = json.load(file)
+
 
 app = Flask(__name__)
 CORS(app)
@@ -25,7 +32,17 @@ CORS(app)
 
 # Sample search for repos in specific lang
 def sample_search(query, lang):
-    return data[lang][query]
+    input_json = dict()
+
+    for l in lang:
+        lang_json = data.get(l)
+        if lang_json:
+            input_json[l] = lang_json
+        else:
+            print(f"NO LANG FOUND {type(lang)}")
+    return ranker(input_json, query).to_json(orient='records')
+
+    
 
 
 @app.route("/")
@@ -36,9 +53,12 @@ def home():
 @app.route("/repo")
 def repo_search():
     repo = request.args.get("repo")
-    lang = request.args.get("lang")
+    lang_str = request.args.get("lang")
+    lang = lang_str.split(',') if lang_str else []
+
     return sample_search(repo, lang)
 
 
 if 'DB_NAME' not in os.environ:
     app.run(debug=True, host="0.0.0.0", port=5000)
+
