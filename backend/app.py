46062af573f9931cedcddf817a10675465adc7e4
Advika <63502073+RAdvika@users.noreply.github.com>
2025-04-14 17:51:57 -0400

    Update app.py sample_data path

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, "..", "db", "sample_data.json")
+json_file_path = os.path.join(current_directory, "sample_data.json")
 
 top_10_langs = {'javascript', 'python', 'java', 'typescript', 'csharp', 'cpp', 'php', 'shell', 'c', 'ruby'}
 with open(json_file_path, 'r') as file:
