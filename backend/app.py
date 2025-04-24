3292c61ac4af4dc75f03b6b60a9cba255520b153
Batchpingu <lamle96@gmail.com>
2025-04-24 04:39:02 -0400

    added meta data ranking to result

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,9 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from typing import List, Tuple
 from helpers.ranker import Ranker
+import numpy as np
+DEMO_MODE = True
+
 
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
@@ -14,6 +17,8 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the path to the JSON file relative to the current script
 json_file_path = os.path.join(current_directory, '..', 'db', "sample_data.json")
+
+
 
 top_10_langs = {'javascript', 'python', 'java', 'typescript', 'csharp', 'cpp', 'php', 'shell', 'c', 'ruby'}
 with open(json_file_path, 'r') as file:
@@ -67,6 +72,7 @@ def format_json(ranked: tuple[int, int, int, float], total: int):
         repo_json = {
             'repo_name': repo.repo_name,
             'language': 'python',
+            'short_desc' : repo.short_desc,
             'readme_raw': readme_text,
             'similarity': sim*100,
             'stars' : repo.stars_count,
@@ -80,13 +86,36 @@ def format_json(ranked: tuple[int, int, int, float], total: int):
 
     return result_json
 
+def meta_data_rank(a = 0.7,
+                   b = 0.1,
+                   y = 0.2, 
+                   scores: list[tuple[int, float]] = None)  -> list[tuple[int, float]]:
+    
+    if scores is None or len(scores) == 0:
+        return []
+    
+    results = []
+
+    for idx, sim in scores:
+        stars = ranker.repositories[idx].stars_count
+        forks = ranker.repositories[idx].forks_count
+
+        stars_score = np.log(1 + stars)
+        forks_score = np.log(1 + forks)
+        rank_score  = a*sim + b*stars_score + y*forks_score 
+
+        results.append(idx,sim,rank_score)
+
+    ranked_result = sorted(results, key=lambda x: x[2], reverse=True)
+
+    return [(idx, sim) for idx, sim, _ in ranked_result]
 
     
 
 
 @app.route("/")
 def home():
-    return render_template('base.html', title="sample html")
+    return render_template('base.html', is_demo_mode=("true" if DEMO_MODE else "false"))
 
 
 @app.route("/repo")
@@ -100,25 +129,12 @@ def repo_search():
     key = f"{repo}_{'_'.join(sorted(lang))}"
     ranked = search(key, repo)
 
-    start = page * per_page
+    start = (page - 1) * per_page
     end = start + per_page
     paginated = ranked[start:end]
 
     return jsonify(format_json(paginated, len(ranked)))
 
-
-
-    # start = (page - 1) * per_page
-    # end = start + per_page
-    # paginated = ranked_results.iloc[start:end]
-
-    # paginated['similarity'] = paginated['similarity'] * 100
-
-    # total =  len(ranked_results)
-    # return {
-    #     "total": total,
-    #     "results" :paginated.to_json(orient='records')
-    # }
 
 
 if 'DB_NAME' not in os.environ:
