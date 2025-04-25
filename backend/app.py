c625d4cdf2ba023e0078f74044052efef05ae332
Advika <63502073+RAdvika@users.noreply.github.com>
2025-04-25 02:59:15 -0400

    Move file to new directory

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 # # Specify the path to the JSON file relative to the current script
 # json_file_path = os.path.join(current_directory, '..', 'db', "sample_data.json")
 
-json_file_path = os.path.join(current_directory, "db/sample_data.json")
+json_file_path = os.path.join(current_directory, "sample_data.json")
 
 
 top_10_langs = {'javascript', 'python', 'java', 'typescript', 'csharp', 'cpp', 'php', 'shell', 'c', 'ruby'}
